090020c13e0d99d1b92d9b489377effeac8a8c85
AndreasHoff <mchoffn@hotmail.com>
2023-10-08 00:18:30 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,25 +15,27 @@ export const Header = () => {
     navigate('/home');
   }
 
-  function register() {
-    navigate('/register');
   } */
 
   return (
     <nav className='navbar bg-light mb-4 p-0 flex'>
-      <div className="container">
-        <a className="navbar-brand" >
-          <div className="d-flex">
-            <img src={logo} alt="logo" className='mr-2' />
+      <div className='container'>
+        <a className='navbar-brand'>
+          <div className='d-flex'>
+            <img
+              src={logo}
+              alt='logo'
+              className='mr-2'
+            />
             <div>Dragons - Rise of Berk</div>
           </div>
         </a>
         <div className='loginButtons'>
-            <button >Register</button>
-            <button >Login</button>
-            <button >Log out</button>
-          </div>
+          <button>Register</button>
+          <button>Login</button>
+          <button>Log out</button>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
