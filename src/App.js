a48c3a239b87f53b92a1975a1fbd319c3392c8e8
AndreasHoff <mchoffn@hotmail.com>
2023-10-08 00:38:03 +0200

    app up n running with background and some buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { Header } from "./components/Header";
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+             <Route path="/home" element={<Home />} />
+             <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+    </>  
   );
 }
 
